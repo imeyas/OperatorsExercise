fe7a46989243352c8e1516491b73e0d057a4973b
Ebo Onwunali <ebo@njgeneral.com>
2022-01-12 00:12:12 -0500

    Initial commit

---
+++ OperatorExercise/OperatorExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The code below is for exercise 1
+            int a = 4;
+            int b = 17;
+            int quotient = b / a;
+            int remainder = b % a;
+
+            Console.WriteLine($"{b} / {a} is {quotient} and the remainder is {remainder}", a, b, quotient, remainder); //Division and Nodulus
+
+            Console.WriteLine(9 * 5); // Multiplication
+
+            Console.WriteLine(11 + 6); // Addition
+
+            Console.WriteLine(11 - 4); // Subtraction
+
+            // The code below is for exercise 2
+            
+        }
+
+    }
+}
